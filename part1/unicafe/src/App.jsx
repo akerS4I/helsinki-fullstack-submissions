6e01bf055635b406a7e4c03b22c8cc885a925c9b
akerS4I <birdblack283474@gmail.com>
2026-06-23 04:56:54 +0500

    part1: task 1.11 submit

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,7 +1,12 @@
 import { useState } from "react";
 
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
-const StatisticLine = ({ text, value }) => <p>{text}: {value}</p>;
+const StatisticLine = ({ text, value }) => (
+  <tr>
+    <td>{text}:</td>
+    <td>{value}</td>
+  </tr>
+);
 
 const Statistics = ({ good, neutral, bad }) => {
   const all = good + neutral + bad;
@@ -9,14 +14,16 @@ const Statistics = ({ good, neutral, bad }) => {
     return <p>No Feedback Yet</p>;
   }
   return (
-    <>
-      <StatisticLine text="Good" value={good}/>
-      <StatisticLine text="Neutral" value={neutral}/>
-      <StatisticLine text="Bad" value={bad}/>
-      <StatisticLine text="All" value={all}/>
-      <StatisticLine text="AVG" value={(good - bad) / all}/>
-      <StatisticLine text="Positive" value={`${(good / all) * 100}%`}/>
-    </>
+    <table>
+      <tbody>
+        <StatisticLine text="Good" value={good} />
+        <StatisticLine text="Neutral" value={neutral} />
+        <StatisticLine text="Bad" value={bad} />
+        <StatisticLine text="All" value={all} />
+        <StatisticLine text="AVG" value={((good - bad) / all).toFixed(1)} />
+        <StatisticLine text="Positive" value={`${((good / all) * 100).toFixed(1)}%`} />
+      </tbody>
+    </table>
   );
 };
 
@@ -28,9 +35,9 @@ const App = () => {
   return (
     <>
       <h1>Give Feedback</h1>
-      <Button onClick={() => setGood((prev) => prev + 1)} text="Good"/>
-      <Button onClick={() => setNeutral((prev) => prev + 1)} text="Neutral"/>
-      <Button onClick={() => setBad((prev) => prev + 1)} text="Bad"/>
+      <Button onClick={() => setGood((prev) => prev + 1)} text="Good" />
+      <Button onClick={() => setNeutral((prev) => prev + 1)} text="Neutral" />
+      <Button onClick={() => setBad((prev) => prev + 1)} text="Bad" />
       <h1>Statistics</h1>
       <Statistics good={good} neutral={neutral} bad={bad} />
     </>
