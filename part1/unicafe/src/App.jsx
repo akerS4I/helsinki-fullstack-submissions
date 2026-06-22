66acaa8b81865fe93063ef8e6ed92ab81b2c3dd9
akerS4I <birdblack283474@gmail.com>
2026-06-23 04:31:08 +0500

    part1: task 1.9 submit

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,15 +1,21 @@
 import { useState } from "react";
 
-const Statistics = ({ good, neutral, bad }) => (
-  <>
-    <p>Good: {good}</p>
-    <p>Neutral: {neutral}</p>
-    <p>Bad: {bad}</p>
-    <p>All: {good + neutral + bad}</p>
-    <p>AVG: {(good - bad) / (good + neutral + bad)}</p>
-    <p>Positive: {(good / (good + neutral + bad)) * 100}%</p>
-  </>
-);
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad;
+  if (all === 0) {
+    return <p>No Feedback Yet</p>;
+  }
+  return (
+    <>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+      <p>All: {all}</p>
+      <p>AVG: {(good - bad) / all}</p>
+      <p>Positive: {(good / all) * 100}%</p>
+    </>
+  );
+};
 
 const App = () => {
   const [good, setGood] = useState(0);
