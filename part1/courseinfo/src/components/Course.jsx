583cf54467b2e44b0f7b2e492d472e601eb01241
akerS4I <birdblack283474@gmail.com>
2026-06-25 03:29:49 +0500

    part2: task 2.5 sumbit

--- part1/courseinfo/src/components/Course.jsx
+++ part1/courseinfo/src/components/Course.jsx
@@ -1,6 +1,20 @@
-import Header from "./Header";
-import Content from "./Content";
-import Total from "./Total";
+const Header = (props) => <h1>{props.course}</h1>;
+
+const Part = (props) => <p>{props.part.name + ": " + props.part.exercises}</p>;
+
+const Content = (props) => (
+  <>
+    {props.parts.map((p) => (
+      <Part part={p} key={p.id} />
+    ))}
+  </>
+);
+
+const Total = ({ parts }) => {
+  const total = parts.reduce((s, p) => s + p.exercises, 0)
+  console.log('reduce method result:', total);
+  return <b>This course has: {total} exercises</b>;
+};
 
 
 const Course = ({ course }) => {
--- part1/courseinfo/src/components/Total.jsx
+++
@@ -1,7 +0,0 @@
-const Total = ({ parts }) => {
-  const total = parts.reduce((s, p) => s + p.exercises, 0)
-  console.log('reduce method result:', total);
-  return <b>This course has: {total} exercises</b>;
-};
-
-export default Total;
