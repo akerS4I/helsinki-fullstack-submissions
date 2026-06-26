2740b44d64e001f190a2ad9d430c2e4d2c497424
akerS4I <birdblack283474@gmail.com>
2026-06-27 04:32:21 +0500

    part2: task 2.11 sumbit

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const Filter = ({ value, onChange }) => (
   <>
@@ -26,29 +27,38 @@ const Form = ({ name, nameChange, number, numberChange, onSubmit }) => (
   </>
 );
 
-const Persons = ({ filteredPersons }) => (
+const Persons = ({ loading, filteredPersons }) => (
   <>
     <h2>Numbers</h2>
-    <ul>
-      {filteredPersons.map((p) => (
-        <li key={p.id}>
-          {p.name}: {p.number}
-        </li>
-      ))}
-    </ul>
+    {loading ? (
+      <p>Loading...</p>
+    ) : (
+      <ul>
+        {filteredPersons.map((p) => (
+          <li key={p.id}>
+            {p.name}: {p.number}
+          </li>
+        ))}
+      </ul>
+    )}
   </>
 );
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
+  const [loading, setLoading] = useState(true);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    console.log("useEffect");
+    axios.get("http://localhost:3001/persons").then((res) => {
+      setPersons(res.data);
+      console.log(res.data);
+      setLoading(false);
+    });
+  }, []);
 
   const handleNameInput = (e) => setNewName(e.target.value);
   const handleNumberInput = (e) => setNewNumber(e.target.value);
@@ -91,7 +101,7 @@ const App = () => {
         numberChange={handleNumberInput}
         onSubmit={handleNewPerson}
       />
-      <Persons filteredPersons={filteredPersons} />
+      <Persons loading={loading} filteredPersons={filteredPersons} />
     </div>
   );
 };
